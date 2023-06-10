abfe2323bea99cf3cb0e6f9adfc69ab09ec61b2a
Marcello Costa <marcello88costa@yahoo.com.br>
2023-06-10 15:00:16 -0300

    Fixing assets

---
+++ _next/static/chunks/app/layout-066a2dbdbe8ef4c5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3227:function(e,n,_){Promise.resolve().then(_.t.bind(_,8583,23))},8583:function(e){e.exports={style:{fontFamily:"'__Inter_0ec1f4', '__Inter_Fallback_0ec1f4'",fontStyle:"normal"},className:"__className_0ec1f4"}}},function(e){e.O(0,[667,488,744],function(){return e(e.s=3227)}),_N_E=e.O()}]);
